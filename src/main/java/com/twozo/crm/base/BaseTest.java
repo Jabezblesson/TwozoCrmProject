151d082a97dcc0e1896cd9e24e89d7a928fca58b
Jabezblesson <Jabezblesson158@gmail.com>
2026-01-05 10:57:55 +0530

    order changed

--- src/main/java/com/twozo/crm/base/BaseTest.java
+++ src/main/java/com/twozo/crm/base/BaseTest.java
@@ -1,9 +1,7 @@
 package com.twozo.crm.base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,12 +12,12 @@ import java.time.Duration;
 
 public class BaseTest {
 
-    protected WebDriver driver;
+    public WebDriver driver;
     protected WebDriverWait wait;
 
     @BeforeMethod
     public void setup() {
-       /* final String browser = ConfigReader.optString("browser");
+        final String browser = ConfigReader.optString("browser");
         final String url = ConfigReader.optString("baseUrl");
         final int timeout = Integer.parseInt(ConfigReader.optString("timeout"));
         if (browser.equalsIgnoreCase("chrome")) {
@@ -29,20 +27,6 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeout));
         wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
-    }*/
-        ChromeOptions options = new ChromeOptions();
-
-        options.addArguments("--headless=new");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(options);
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
     }
 
     @AfterMethod
